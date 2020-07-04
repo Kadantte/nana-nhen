a8547a92bdc8b4870b75445a260c3066db504e1a
Akizuki Masami <masami4501@gmail.com>
2020-07-04 18:10:56 +0700

    Update and rename favorite.js.bak to favorite.js

---
+++ commands/favorite.js
@@ -0,0 +1,23 @@
+exports.run = async (client, msg, args, color) => {
+  if (!msg.channel.nsfw)
+    return msg.channel
+      .send(`NSFW channel please.`)
+      .then(msg => msg.delete({ timeout: 5000 }));
+  let nick =
+    msg.member.nickname !== null
+      ? `${msg.member.nickname}`
+      : msg.author.username;
+
+  msg.channel.send(`Sorry **${nick}** but for the time being, favorite commands can only be accessed via **Nana Beta** on nana's server`);
+};
+
+exports.conf = {
+  aliases: ["favorites", "fav"],
+  cooldown: 3
+};
+
+exports.help = {
+  name: "favorite",
+  description: "Safe your favorite doujin ID",
+  usage: ["favorite add <BookID>", "favorite delete <BookID>"]
+};
