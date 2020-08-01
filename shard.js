b1300ab593bbad8d7333dc443b5be0139dad3dfb
masami45 <masami4501@gmail.com>
2020-08-01 19:23:05 +0700

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require("discord.js");
 const manager = new ShardingManager("./app.js", { token: process.env.TOKEN });
 
-manager.spawn();
+manager.spawn(2);
 manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}`));
