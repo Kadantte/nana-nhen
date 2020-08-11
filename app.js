850cd92df4d2afee9d79af54175973254964d7f9
masami45 <masami4501@gmail.com>
2020-08-11 18:36:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const client = new Nana({
   cacheGuilds: true,
   cacheChannels: true,
   fetchAllMembers: true,
-  disableEvents: ["GUILD_SYNC", "PRESENCE_UPDATE", "TYPING_START"]
+  messageCacheMaxSize: 0
 });
 
 // events
