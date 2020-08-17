71de871cb202fc980b7305f90f5fbb3b1fa4d3b3
masami45 <masami4501@gmail.com>
2020-08-17 08:56:25 +0700

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -4,9 +4,9 @@ const pkg = require("../package.json");
 exports.run = async (client, msg, args, color) => {
   const uptime = client.util.parseDur(client.uptime);
   const botVersion = pkg.version;
-  const usersTotal = await client.util.fetchClientValues("users.cache.size");
+  const usersTotal = await client.util.getShardTotal("users.cache.size");
   const users = await client.shard.fetchClientValues("users.cache.size");
-  const channelsTotal = await client.util.fetchClientValues("channels.cache.size");
+  const channelsTotal = await client.util.getShardTotal("channels.cache.size");
   const channels = await client.shard.fetchClientValues("channels.cache.size");
   const serversTotal = await client.util.getShardTotal("guilds.cache.size");
   const servers = await client.shard.fetchClientValues("guilds.cache.size");
