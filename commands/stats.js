80a1856dc3ad3dbd76b5b970fcc54d7acfb00435
masami45 <masami4501@gmail.com>
2020-08-17 08:19:58 +0700

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -4,76 +4,24 @@ const pkg = require("../package.json");
 exports.run = async (client, msg, args, color) => {
   const uptime = client.util.parseDur(client.uptime);
   const botVersion = pkg.version;
-  const users = await client.util.getShardTotal("users.cache.size");
-  const channels = await client.util.getShardTotal("channels.cache.size");
-  const servers = await client.util.getShardTotal("guilds.cache.size");
+  const usersTotal = await client.shard.fetchClientValues("guilds.cache.size");
+  const users = await client.shard.fetchClientValues("users.cache.size");
+  const channelsTotal = await client.shard.fetchClientValues("guilds.cache.size");
+  const channels = await client.shard.fetchClientValues("channels.cache.size");
+  const serversTotal = await client.util.getShardTotal("guilds.cache.size");
+  const servers = await client.shard.fetchClientValues("guilds.cache.size");
 
-  // if (!args[0]) {
   msg.channel.send(`\`\`\`asciidoc
 Mem. Usage :: ${Math.floor(process.memoryUsage().heapUsed / 1048576)} MB
 Uptime     :: ${uptime}
 WS Ping    :: ${client.ws.ping}ms
-Users      :: ${users.toLocaleString()}
-Servers    :: ${servers.toLocaleString()}
-Channels   :: ${channels.toLocaleString()}
+Users      :: ${users[client.shard.ids].toLocaleString()}/${usersTotal.toLocaleString()}
+Channels   :: ${channels[client.shard.ids].toLocaleString()}/${channelsTotal.toLocaleString()}
+Servers    :: ${servers[client.shard.ids].toLocaleString()}/${serversTotal.toLocaleString()}
+Shards     :: ${parseInt(client.shard.ids) + 1} of ${client.shard.count}
 Bot Vers.  :: ${botVersion}
 D.js-light :: v${version}
 Node       :: ${process.version}\`\`\``);
-  //   } else if (args[0] == "server") {
-  //     if (!client.config.OWNERS.includes(msg.author.id)) return;
-
-  //     let guildsCount = [];
-  //     let servers = await client.shard.broadcastEval(
-  //       "this.guilds.cache.array().slice().map(x => x)"
-  //     );
-  //     servers.forEach(e => {
-  //       e.sort((a, b) =>
-  //         a.memberCount < b.memberCount
-  //           ? 1
-  //           : b.memberCount < a.memberCount
-  //           ? -1
-  //           : 0
-  //       ).forEach(server => {
-  //         for (var [i, x] of server.entries()) {
-  //           guildsCount.push(`\`${i + 1}\`. ${x.name} = \`${x.memberCount}\``);
-  //         }
-  //       });
-  //     });
-
-  //     guildsCount = client.util.chunk(guildsCount, 15);
-  //     let page = 1;
-  //     const embed = new MessageEmbed()
-  //       .setColor(color)
-  //       .setFooter(`Page ${page} of ${guildsCount.length}`)
-  //       .setDescription(guildsCount[page - 1]);
-  //     let m = await msg.channel.send(embed);
-
-  //     await m.react(`◀`);
-  //     await m.react(`▶`);
-
-  //     const backwardsFilter = (reaction, user) =>
-  //       reaction.emoji.name === `◀` && user.id === msg.author.id;
-  //     const forwardsFilter = (reaction, user) =>
-  //       reaction.emoji.name === `▶` && user.id === msg.author.id;
-  //     const backwards = m.createReactionCollector(backwardsFilter);
-  //     const forwards = m.createReactionCollector(forwardsFilter);
-
-  //     backwards.on("collect", r => {
-  //       if (page === 1) return;
-  //       page--;
-  //       embed.setDescription(guildsCount[page - 1]);
-  //       embed.setFooter(`Page ${page} of ${guildsCount.length}`);
-  //       m.edit(embed);
-  //     });
-
-  //     forwards.on("collect", r => {
-  //       if (page === guildsCount.length) return;
-  //       page++;
-  //       embed.setDescription(guildsCount[page - 1]);
-  //       embed.setFooter(`Page ${page} of ${guildsCount.length}`);
-  //       m.edit(embed);
-  //     });
-  //   }
 };
 
 exports.conf = {
